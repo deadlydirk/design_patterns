65b2bdb93b85207eeec3b446f201434227a1291b
Joris Aper <aper.joris@gmail.com>
2014-02-03 11:42:13 +0100

    Create Address.java

---
+++ Address.java
@@ -0,0 +1,22 @@
+
+public class Address {
+
+
+    private String street;
+
+    private String number;
+
+    private String zipCode;
+
+    private String city;
+
+    private String state;
+
+    private String country;
+
+
+    //getters and setters removed for brevity
+
+  
+
+}
